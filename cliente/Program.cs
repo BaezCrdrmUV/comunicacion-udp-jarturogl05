39769ac40486ca38ba82b34e55761fc062d2f7c9
arturo <jarturogl05@gmail.com>
2020-04-03 20:12:17 -0600

    Programa

---
+++ cliente/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace cliente
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            UdpClient udpClient = new UdpClient();
+            udpClient.Connect("192.168.1.76", 8080);
+
+            bool serverStatus = true;
+
+            while (serverStatus)
+            {
+
+                Console.WriteLine("Escribe un mensaje");
+                string msg = Console.ReadLine();
+
+                if (msg == "bye")
+                {
+                    Console.WriteLine("adios");
+                    serverStatus = false;
+                }
+                else
+                {
+                    Byte[] senddata = System.Text.Encoding.UTF8.GetBytes(msg);
+                    udpClient.Send(senddata, senddata.Length);
+                }
+
+            }
+
+            udpClient.Close();
+        }
+    }
+}
---
+++ server/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace server
+{
+    public class UdpServer
+    {
+        public static void Main()
+        {
+            byte[] data = new byte[1024];
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Any, 8080);
+            UdpClient newsock = new UdpClient(ipep);
+
+            Console.WriteLine("esperando cliente...");
+
+            IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+            bool serverStatus = true;
+
+            while (serverStatus)
+            {
+                data = newsock.Receive(ref sender);
+                Console.WriteLine("Mensaje recibido de {0}:", sender.ToString());
+                string msj = Encoding.UTF8.GetString(data, 0, data.Length);
+                if (msj == "bye")
+                {
+                    newsock.Close();
+
+                }
+                else
+                {
+
+                    Console.WriteLine(msj);
+                }
+
+            }
+
+        }
+    }
+}
